0afcadace44587b2ed05fcab66edc86193f9252a
Njalale <fnjalale@gmail.com>
2023-03-14 02:56:10 +0300

    Commit assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+function multiply(){
+    return num1 * num2;
+}
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) +1
+function random(){
+   return Math.floor(Math.random() * 10) +1;
+}
+let num3 = 29
+let num4 = 5
+let mod = num3 % num4
+function mod(){
+    return num3 % num4
+}
+
+let max = 20
+function max(){
+    return Math.max(0, 5, 10, 15, 20)
+}
